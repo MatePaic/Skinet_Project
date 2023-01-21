0119f1e59828bda34c4426c3cba5a654fe795afe
matepaic <mate.paic1997@gmail.com>
2023-01-22 00:36:12 +0100

    Identity added

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,10 @@
 using API.Extensions;
 using API.Helpers;
 using API.Middleware;
+using Core.Entities.Identity;
 using Infrastructure.Data;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
 
@@ -15,12 +18,20 @@ builder.Services.AddDbContext<StoreContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection"));
+});
+
 builder.Services.AddSingleton<IConnectionMultiplexer>(c =>
 {
 	var configuration = ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("Redis"), true);
 	return ConnectionMultiplexer.Connect(configuration);
 });
+
 builder.Services.AddApplicationServices();
+builder.Services.AddIdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSwaggerDocumentation();
 
@@ -45,6 +56,11 @@ using (var scope = app.Services.CreateScope())
 		var context = services.GetRequiredService<StoreContext>();
 		await context.Database.MigrateAsync();
 		await StoreContextSeed.SeedAsync(context, loggerFactory);
+
+		var userManager = services.GetRequiredService<UserManager<AppUser>>();
+		var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+		await identityContext.Database.MigrateAsync();
+		await AppIdentityDbContextSeed.SeedUserAsync(userManager);
 	}
 	catch (Exception ex)
 	{
@@ -66,6 +82,7 @@ app.UseStaticFiles();
 
 app.UseCors("CorsPolicy");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseSwaggerDocumentation();
