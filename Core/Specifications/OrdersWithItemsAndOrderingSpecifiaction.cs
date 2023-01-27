a0d3f1482696864da3aef55f6280b79882457d82
matepaic <mate.paic1997@gmail.com>
2023-01-27 20:56:38 +0100

    Api-Orders

---
+++ Core/Specifications/OrdersWithItemsAndOrderingSpecifiaction.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities.OrderAggregate;
+using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    public class OrdersWithItemsAndOrderingSpecifiaction : BaseSpecification<Order>
+    {
+        public OrdersWithItemsAndOrderingSpecifiaction(string email) : base(o => o.BuyerEmail == email)
+        {
+            AddInclude(o => o.OrderItems);
+            AddInclude(o => o.DeliveryMethod);
+            AddOrderByDescending(o => o.OrderDate);
+        }
+
+        public OrdersWithItemsAndOrderingSpecifiaction(int id, string email) : base(o => o.Id == id && o.BuyerEmail == email)
+        {
+            AddInclude(o => o.OrderItems);
+            AddInclude(o => o.DeliveryMethod);
+        }
+    }
+}
